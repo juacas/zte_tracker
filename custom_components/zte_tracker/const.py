8e6be3fc9913bd244f166104d99c4d89168205c8
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2024-05-03 15:47:28 +0000

    Fix #19 (Compatibility HA2024.5)

--- custom_components/zte_tracker/const.py
+++ custom_components/zte_tracker/const.py
@@ -5,7 +5,7 @@ from unittest.mock import DEFAULT
 
 DOMAIN = "zte_tracker"
 DOMAIN_DATA = "{}_data".format(DOMAIN)
-VERSION = "1.3.7"
+VERSION = "1.3.9"
 PLATFORMS = ["sensor", "device_tracker"]
 ISSUE_URL = "https://github.com/juacas/zte_tracker/issues"
 DEFAULT_HOST = '192.168.1.1'
